f0b0a38bee1a97197df9bd54406737c80ba32a9e
David Hu <coolbor@gmail.com>
2020-03-20 22:18:29 +0800

    done

--- cmd/root.go
+++ cmd/root.go
@@ -17,8 +17,15 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/spf13/cobra"
+	"io/ioutil"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	"github.com/rwcarlsen/goexif/exif"
+	"github.com/rwcarlsen/goexif/mknote"
+	"github.com/spf13/cobra"
 
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/viper"
@@ -26,19 +33,114 @@ import (
 
 var cfgFile string
 
+var path string
+
+var target string
+
+var exts []string
+var depth int
+var preview bool
+
+const DEPTH = 100
+
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "photo-tool",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "Organize photos with EXIF",
+	Long: `Organize photos with EXIF. For example:
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+photo-tool -p youpath -t targetpath -d 1 -e JPG -e PNG -e JPEG`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	RunE: func(cmd *cobra.Command, args []string) error {
+		_, err := os.Stat(path)
+		if err != nil {
+			return err
+		}
+		if depth == 0 {
+			return filepath.Walk(path, walkpath)
+		} else {
+			walkdir(path, 1)
+		}
+		return nil
+	},
+}
+
+func walkdir(p string, d int) {
+	if d > depth || d > DEPTH {
+		return
+	}
+
+	fs, err := ioutil.ReadDir(p)
+	if err != nil {
+		return
+	}
+
+	for _, fi := range fs {
+		if fi.IsDir() {
+			walkdir(filepath.Join(p, fi.Name()), d+1)
+			continue
+		} else {
+			move(filepath.Join(p, fi.Name()), fi)
+		}
+	}
+}
+
+func walkpath(p string, f os.FileInfo, err error) error {
+	if f.IsDir() {
+		return nil
+	}
+
+	if !strings.Contains(strings.ToUpper(strings.Join(exts, ",")), strings.ToUpper(strings.Trim(filepath.Ext(p), "."))) {
+		return nil
+	}
+
+	move(p, f)
+
+	return nil
+}
+
+func move(f string, fi os.FileInfo) {
+	if strings.HasPrefix(fi.Name(), ".") {
+		return
+	}
+	fr, err := os.Open(f)
+	if err != nil {
+		return
+	}
+	defer fr.Close()
+
+	exif.RegisterParsers(mknote.All...)
+	x, e := exif.Decode(fr)
+	if e != nil {
+		return
+	}
+	d, e := x.DateTime()
+	if e != nil {
+		return
+	}
+	folder := d.Format("2006-01-02")
+	tp := filepath.Join(target, folder)
+	// fmt.Println("move", f, "to", filepath.Join(tp, fi.Name()))
+	if !preview {
+		cmd := exec.Command("mkdir", "-p", tp)
+		if _, e := cmd.Output(); e == nil {
+			cmd := exec.Command("mv", f, filepath.Join(tp, fi.Name()))
+			if _, er := cmd.Output(); er != nil {
+				fmt.Println("mv error:", er)
+			}
+		} else {
+			fmt.Println("mkdir error:", e)
+		}
+		// if e := os.MkdirAll(tp, os.ModePerm); e == nil {
+		// 	if er := os.Rename(f, filepath.Join(tp, fi.Name())); er != nil {
+		// 		fmt.Println("rename error:", er)
+		// 	}
+		// } else {
+		// 	fmt.Println("mkdir error:", e)
+		// }
+	}
+	fmt.Printf("move %s to %s complate!\n", f, filepath.Join(tp, fi.Name()))
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -61,7 +163,14 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// rootCmd.Flags().StringP("path", "p", ".", "Scan path (default is current path")
+	rootCmd.Flags().StringVarP(&path, "path", "p", "", "Scan path")
+	rootCmd.Flags().StringVarP(&target, "target", "t", ".", "Target path")
+	defaultExts := []string{"JPG", "JPEG"}
+	rootCmd.Flags().StringArrayVarP(&exts, "ext", "e", defaultExts, "File extensions")
+	rootCmd.Flags().IntVarP(&depth, "depth", "d", 1, "Scan depth")
+	rootCmd.Flags().BoolVar(&preview, "preview", false, "Preview, not really executed!")
 }
 
 // initConfig reads in config file and ENV variables if set.
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.14
 
 require (
 	github.com/mitchellh/go-homedir v1.1.0
-	github.com/rwcarlsen/goexif v0.0.0-20190401172101-9e8deecbddbd // indirect
+	github.com/rwcarlsen/goexif v0.0.0-20190401172101-9e8deecbddbd
 	github.com/spf13/cobra v0.0.6
 	github.com/spf13/viper v1.6.2
 )
